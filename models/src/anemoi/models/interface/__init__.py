64a998392573315475f4a70846364bc1be1c5c57
MeluXina user <u101957@login03.meluxina.lxp.lu>
2026-04-21 09:41:11 +0200

    change inf param

--- models/src/anemoi/models/interface/__init__.py
+++ models/src/anemoi/models/interface/__init__.py
@@ -146,12 +146,12 @@ class AnemoiModelInterface(torch.nn.Module):
         }
         kwargs = {
         "params_inference" : {
-        'sigma_max': 20.0,
+        'sigma_max': 100.0,
         'sigma_min': 0.02,
         'rho': 7,
         'num_steps': 100,
         'num_steps_sdedit': 17,
-        'SDEdit': True
+        'SDEdit': False
         }
         }
 
